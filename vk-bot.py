b48a3ec253884a42cb6c6cb9fe402822969cf43c
Seva Pavlov <sevapav21@gmail.com>
2024-08-08 11:22:41 +0300

    урок 4 предпоследний

--- vk-bot.py
+++ vk-bot.py
@@ -1,3 +1,5 @@
+import time
+import random
 from vk_api.longpoll import VkLongPoll, VkEventType
 from datetime import datetime
 from vk_api import VkApi
@@ -14,11 +16,45 @@ while True:
             print(f'Текст сообщения: {str(event.text)}')
             responce = event.text.lower()
             if event.from_user and not event.from_me:
-                 if responce == 'Привет!':
-                    session .method(
+                if responce.find("привет") >= 0 or responce.find("здравствуй") >= 0:
+                    time.sleep(random.uniform(0.5, 3))
+                    session.method(
                         "messages.send",
                         {
                             "user_id": "И тебе привет!",
                             "random_id": 0,
                         },
-                    )   
+                    )
+                elif responce.find("пока") >= 0 or responce.find("до свидания") >= 0:
+                    time.sleep(random.uniform(0.5, 3))
+                    session.method(
+                        "messages.send",
+                        {
+                            "user_id": "bye bye",
+                            "random_id": 0,
+                        },
+                    )
+
+                elif responce.find("как дела") >= 0 or responce.find("как жизнь?") >= 0:
+                    time.sleep(random.uniform(0.5, 3))
+                    session.method(
+                        "messages.send",
+                        {
+                            "user_id": "Хорошо, спасибо!",
+                            "random_id": 0,
+                        },
+                    )
+                elif responce.find("грустно") >= 0 or responce.find("плохо") >= 0:
+                    time.sleep(random.uniform(0.5, 3))
+                    session.method(
+                        "messages.send",
+                        {
+                            "user_id": event.user_id,
+                            "sticker_id": 73070,
+                            "random_id": 0,
+                        },
+                    )
+                elif responce == "кот":
+                    session.method('messages.send',{'user_id': event.user_id, 'random_id':0, 'attachment': 'photo-28905875_459432012'})
+
+
